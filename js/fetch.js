5bc087aa4b4611c251d9cab202c6930ef4f91a2f
AdeCandra12 <ade.can321@gmail.com>
2023-03-31 16:47:20 +0700

    absensi

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,3 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
-import { setInner } from "https://jscroot.github.io/element/croot.js";
 import { get } from "https://bukulapak.github.io/api/process.js"; 
 let urlAPI = "https://pemrog3-ulbi.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
