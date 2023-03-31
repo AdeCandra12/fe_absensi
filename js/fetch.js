75673bcd938da8649701138174926322e12e24e3
AdeCandra12 <ade.can321@gmail.com>
2023-03-31 16:52:05 +0700

    absensi

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,17 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://pemrog3-ulbi.herokuapp.com/presensi";
+// import { get } from "https://bukulapak.github.io/api/process.js"; 
+// let urlAPI = "https://pemrog3-ulbi.herokuapp.com/presensi";
+// get(urlAPI,isiTablePresensi);
+// function isiTablePresensi(results){
+//   console.log(results);
+//    results.forEach(isiRow);
+// }
+// function isiRow(value){
+//    console.log(value)
+// }
+
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
+let urlAPI = "https://adecandra12.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
