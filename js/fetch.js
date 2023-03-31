17a61cf445fed7f269db414340fb999e41daf549
AdeCandra12 <ade.can321@gmail.com>
2023-03-31 16:38:58 +0700

    absen

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,7 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-let urlAPI = "https://gocroot.herokuapp.com/presensi";
+import { get } from "https://bukulapak.github.io/api/process.js"; 
+let urlAPI = "https://pemrog3-ulbi.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
