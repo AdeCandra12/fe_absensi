7941c8359a956a06cbc8fba7913ecbfc96308ca6
AdeCandra12 <ade.can321@gmail.com>
2023-03-31 17:05:16 +0700

    absensi

--- js/fetch.js
+++ js/fetch.js
@@ -9,14 +9,36 @@
 //    console.log(value)
 // }
 
+// import { get } from "https://bukulapak.github.io/api/process.js";
+// import { setInner } from "https://bukulapak.github.io/element/process.js";
+// let urlAPI = "https://adecandra12.herokuapp.com/presensi";
+// get(urlAPI,isiTablePresensi);
+// function isiTablePresensi(results){
+//     console.log(results);
+//     results.forEach(isiRow);
+// }
+// function isiRow(value){
+//     console.log(value)
+// }
+
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { setInner } from "https://bukulapak.github.io/element/process.js";
-let urlAPI = "https://adecandra12.herokuapp.com/presensi";
-get(urlAPI,isiTablePresensi);
-function isiTablePresensi(results){
-    console.log(results);
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
+let urlAPI = "https://ws-ulbi.herokuapp.com/presensi";
+get(urlAPI, isiTablePresensi);
+function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
-function isiRow(value){
-    console.log(value)
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#NAMA#", value.biodata.nama)
+            .replace("#NOHP#", value.biodata.phone_number)
+            .replace("#JABATAN#", value.biodata.jabatan)
+            .replace("#LOKASI#", value.location)
+            .replace("#STATUS#", value.checkin)
+            .replace("#HARIKERJA#", value.biodata.hari_kerja)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
