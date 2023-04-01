a814f06a61cd4bd36eee7d7c605c17210d2deab9
AdeCandra12 <ade.can321@gmail.com>
2023-04-01 12:16:51 +0700

    absen

--- js/fetch.js
+++ js/fetch.js
@@ -1,26 +1,3 @@
-// import { get } from "https://bukulapak.github.io/api/process.js"; 
-// let urlAPI = "https://pemrog3-ulbi.herokuapp.com/presensi";
-// get(urlAPI,isiTablePresensi);
-// function isiTablePresensi(results){
-//   console.log(results);
-//    results.forEach(isiRow);
-// }
-// function isiRow(value){
-//    console.log(value)
-// }
-
-// import { get } from "https://bukulapak.github.io/api/process.js";
-// import { setInner } from "https://bukulapak.github.io/element/process.js";
-// let urlAPI = "https://adecandra12.herokuapp.com/presensi";
-// get(urlAPI,isiTablePresensi);
-// function isiTablePresensi(results){
-//     console.log(results);
-//     results.forEach(isiRow);
-// }
-// function isiRow(value){
-//     console.log(value)
-// }
-
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
