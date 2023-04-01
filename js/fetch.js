1469a53a981c7f8bb36788f998355b0738a27551
AdeCandra12 <ade.can321@gmail.com>
2023-04-01 13:31:52 +0700

    komen

--- js/fetch.js
+++ js/fetch.js
@@ -17,9 +17,6 @@ function isiRow(value) {
             .replace("#LOKASI#", value.location)
             .replace("#STATUS#", value.checkin)
             .replace("#HARIKERJA#", value.biodata.hari_kerja)
-            .replace("#JAMKERJA#", value.biodata.jam_kerja)
-            .replace("#JAMMASUK#", value.biodata.jam_masuk)
-            .replace("#JAMKELUAR#", value.biodata.jam_keluar)
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
         addInner("iniTabel", content);
