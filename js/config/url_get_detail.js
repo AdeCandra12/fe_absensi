21a113171d215be89a8ea0f38128f2dfec326081
AdeCandra12 <ade.can321@gmail.com>
2023-05-19 15:29:49 +0700

    push

---
+++ js/config/url_get_detail.js
@@ -0,0 +1,4 @@
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get('presensiId');
+
+export let urlFetch = 'https://pemrog3-ulbi.herokuapp.com/presensi' + presensiId;
